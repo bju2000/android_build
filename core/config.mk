7071ce7cf20946bb7ef85486330b47f83f1755ed
Lolo <elpoisot@hotmail.fr>
2020-05-15 12:33:16 +0200

    Always include LineageOS sepolicy

--- core/config.mk
+++ core/config.mk
@@ -1143,11 +1143,9 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
-ifneq ($(LINEAGE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/lineage/sepolicy/common/sepolicy.mk)
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
