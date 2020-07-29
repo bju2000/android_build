aee2efe9b6106bd993a63f8b6b54ececda3fd939
Loïc Poisot <elpoisot@hotmail.fr>
2020-07-29 10:26:37 +0200

    removed the linage checks as we build maru but want lineage functionality

--- envsetup.sh
+++ envsetup.sh
@@ -137,12 +137,6 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^lineage_") ; then
-        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
-    else
-        LINEAGE_BUILD=
-    fi
-    export LINEAGE_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
